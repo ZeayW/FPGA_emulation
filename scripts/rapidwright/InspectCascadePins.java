2f6f84c3ea9e7b30e3348f32cc48291d35d8b751
ZeayW <48197878+ZeayW@users.noreply.github.com>
2026-09-26 04:18:22 +0800

    tools: inspect cascade arcs in both directions

--- scripts/rapidwright/InspectCascadePins.java
+++ scripts/rapidwright/InspectCascadePins.java
@@ -24,6 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.LinkedHashSet;
 
 public final class InspectCascadePins {
     private InspectCascadePins() {}
@@ -46,9 +47,88 @@ public final class InspectCascadePins {
         return tile.getName() + "/" + wire + "/" + tile.getWireName(wire);
     }
 
+    private static void auditSite(Site selected, Map<String, List<String>> nodeOwners) {
+        String prefix = selected.getName().replaceFirst("_X.*$", "_");
+        System.out.println("SITE\t" + prefix + "\t" + selected.getName());
+        for (Site tileSite : selected.getTile().getSites()) {
+            System.out.println("TILESITE\t" + selected.getTile().getName()
+                    + "\t" + tileSite.getSiteIndexInTile()
+                    + "\t" + tileSite.getName()
+                    + "\t" + tileSite.getSiteTypeEnum());
+        }
+        Set<String> auditedDownhill = new HashSet<>();
+        Set<String> auditedUphill = new HashSet<>();
+        for (BEL bel : selected.getBELs()) {
+            for (BELPin pin : bel.getPins()) {
+                if (!cascadeName(pin.getName())) continue;
+                String sitePin = pin.getConnectedSitePinName();
+                Node external = pin.getExternalNode(selected);
+                SitePin nativeSitePin = pin.getSitePin(selected);
+                System.out.println("PIN\t" + selected.getName()
+                        + "\t" + bel.getName()
+                        + "\t" + pin.getName()
+                        + "\t" + (pin.isInput() ? "I" : "O")
+                        + "\t" + (sitePin == null ? "-" : sitePin)
+                        + "\t" + nodeKey(external)
+                        + "\t" + (nativeSitePin == null ? "-"
+                            : Boolean.toString(
+                                nativeSitePin.getBELPin().isDedicatedSitePin())));
+                for (com.xilinx.rapidwright.device.SitePIP sitePip : pin.getSitePIPs()) {
+                    System.out.println("SITEPIP\t" + selected.getName()
+                            + "\t" + bel.getName() + "/" + pin.getName()
+                            + "\t" + sitePip.getName(selected)
+                            + "\t" + sitePip.getInputPinName()
+                            + "\t" + sitePip.getOutputPinName());
+                }
+                for (BELPin connection : pin.getSiteConns()) {
+                    System.out.println("SITECONN\t" + selected.getName()
+                            + "\t" + bel.getName() + "/" + pin.getName()
+                            + "\t" + connection.getBELName() + "/"
+                            + connection.getName());
+                }
+                if (pin.isOutput() && external != null
+                        && auditedDownhill.add(nodeKey(external))) {
+                    for (PIP pip : external.getAllDownhillPIPs()) {
+                        Node end = pip.getEndNode();
+                        SitePin sink = end == null ? null : end.getSitePin();
+                        System.out.println("DOWNPIP\t" + selected.getName()
+                                + "\t" + nodeKey(external)
+                                + "\t" + pip.getPIPType()
+                                + "\t" + pip.isPIPFixed()
+                                + "\t" + nodeKey(end)
+                                + "\t" + (sink == null ? "-"
+                                    : sink.getSite().getName() + "/"
+                                        + sink.getPinName())
+                                + "\t" + String.join(",",
+                                    nodeOwners.getOrDefault(
+                                        nodeKey(end), new ArrayList<>())));
+                    }
+                }
+                if (pin.isInput() && external != null
+                        && auditedUphill.add(nodeKey(external))) {
+                    for (PIP pip : external.getAllUphillPIPs()) {
+                        Node start = pip.getStartNode();
+                        SitePin source = start == null ? null : start.getSitePin();
+                        System.out.println("UPPIP\t" + selected.getName()
+                                + "\t" + nodeKey(external)
+                                + "\t" + pip.getPIPType()
+                                + "\t" + pip.isPIPFixed()
+                                + "\t" + nodeKey(start)
+                                + "\t" + (source == null ? "-"
+                                    : source.getSite().getName() + "/"
+                                        + source.getPinName())
+                                + "\t" + String.join(",",
+                                    nodeOwners.getOrDefault(
+                                        nodeKey(start), new ArrayList<>())));
+                    }
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        if (args.length != 1) {
-            throw new IllegalArgumentException("usage: <full-part>");
+        if (args.length < 1) {
+            throw new IllegalArgumentException("usage: <full-part> [site ...]");
         }
         Device device = Device.getDevice(args[0]);
         Map<String, List<String>> nodeOwners = new HashMap<>();
@@ -66,60 +146,26 @@ public final class InspectCascadePins {
                 }
             }
         }
-        String[] prefixes = {"DSP48E2_", "RAMB36_", "URAM288_"};
-        for (String prefix : prefixes) {
-            Site selected = null;
-            for (Site site : device.getAllSites()) {
-                if (site != null && site.getName().startsWith(prefix)) {
-                    selected = site;
-                    break;
+        LinkedHashSet<Site> selected = new LinkedHashSet<>();
+        if (args.length > 1) {
+            for (int index = 1; index < args.length; ++index) {
+                Site site = device.getSite(args[index]);
+                if (site == null) {
+                    throw new IllegalArgumentException("unknown site " + args[index]);
                 }
+                selected.add(site);
             }
-            System.out.println("SITE\t" + prefix + "\t"
-                    + (selected == null ? "-" : selected.getName()));
-            if (selected == null) continue;
-            for (Site tileSite : selected.getTile().getSites()) {
-                System.out.println("TILESITE\t" + selected.getTile().getName()
-                        + "\t" + tileSite.getSiteIndexInTile()
-                        + "\t" + tileSite.getName()
-                        + "\t" + tileSite.getSiteTypeEnum());
-            }
-            Set<String> auditedNodes = new HashSet<>();
-            for (BEL bel : selected.getBELs()) {
-                for (BELPin pin : bel.getPins()) {
-                    if (!cascadeName(pin.getName())) continue;
-                    String sitePin = pin.getConnectedSitePinName();
-                    Node external = pin.getExternalNode(selected);
-                    SitePin nativeSitePin = pin.getSitePin(selected);
-                    System.out.println("PIN\t" + selected.getName()
-                            + "\t" + bel.getName()
-                            + "\t" + pin.getName()
-                            + "\t" + (pin.isInput() ? "I" : "O")
-                            + "\t" + (sitePin == null ? "-" : sitePin)
-                            + "\t" + nodeKey(external)
-                            + "\t" + (nativeSitePin == null ? "-"
-                                : Boolean.toString(
-                                    nativeSitePin.getBELPin().isDedicatedSitePin())));
-                    if (pin.isOutput() && external != null
-                            && auditedNodes.add(nodeKey(external))) {
-                        for (PIP pip : external.getAllDownhillPIPs()) {
-                            Node end = pip.getEndNode();
-                            SitePin sink = end == null ? null : end.getSitePin();
-                            System.out.println("PIP\t" + selected.getName()
-                                    + "\t" + nodeKey(external)
-                                    + "\t" + pip.getPIPType()
-                                    + "\t" + pip.isPIPFixed()
-                                    + "\t" + nodeKey(end)
-                                    + "\t" + (sink == null ? "-"
-                                        : sink.getSite().getName() + "/"
-                                            + sink.getPinName())
-                                    + "\t" + String.join(",",
-                                        nodeOwners.getOrDefault(
-                                            nodeKey(end), new ArrayList<>())));
-                        }
+        } else {
+            String[] prefixes = {"DSP48E2_", "RAMB36_", "URAM288_"};
+            for (String prefix : prefixes) {
+                for (Site site : device.getAllSites()) {
+                    if (site != null && site.getName().startsWith(prefix)) {
+                        selected.add(site);
+                        break;
                     }
                 }
             }
         }
+        for (Site site : selected) auditSite(site, nodeOwners);
     }
 }
